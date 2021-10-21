eb6fd46bad23f14efcb7e90e22d2ab25f0e6154f
Briana Endanawas <brianamay48@gmail.com>
2021-10-20 23:58:55 -0400

    array tail

---
+++ tail.js
@@ -0,0 +1,27 @@
+let errorMsg;
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    errorMsg = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+    console.log(errorMsg);
+  } else if (actual === expected) {
+    errorMsg = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+    console.log(errorMsg);
+  }
+};
+
+let newArray = [];
+const tail = function(array) {
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
