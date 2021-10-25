306a4998f8eafd25d330ba7194cf230f3e70d175
Briana Endanawas <brianamay48@gmail.com>
2021-10-24 21:00:25 -0400

    implement assert

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+let errorMsg;
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    errorMsg = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+    console.log(errorMsg);
+  } else if (actual === expected) {
+    errorMsg = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+    console.log(errorMsg);
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  if (JSON.stringify(firstArray) === JSON.stringify(secondArray)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true);
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
