51abe5a7fe943790783187412ed51888f221388d
Briana Endanawas <brianamay48@gmail.com>
2021-10-28 01:21:42 -0400

    find key

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+let errorMsg;
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    errorMsg = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+    console.log(errorMsg);
+  } else if (actual === expected) {
+    errorMsg = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+    console.log(errorMsg);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  //let keys = Object.keys(object);
+  for (const key in object) {
+    //console.log(k);
+    if (object.hasOwnProperty(key)) {
+      if (object[key] === value) {
+        return key;
+      }
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), "undefined");
