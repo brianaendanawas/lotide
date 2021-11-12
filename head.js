a236e228b8f5b1a902a3e11ca207026ccefa8149
Briana Endanawas <brianamay48@gmail.com>
2021-11-12 06:39:45 -0500

    cleanup

--- head.js
+++ head.js
@@ -1,13 +1,3 @@
-let errorMsg;
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    errorMsg = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-    console.log(errorMsg);
-  } else if (actual === expected) {
-    errorMsg = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-    console.log(errorMsg);
-  }
-}; 
 
 //assertEqual("Lighthouse Labs", "Bootcamp");
 //assertEqual(1, 1);
@@ -16,5 +6,4 @@ const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
