feadde8e49961aeae826f2cb8a3caf6c112bc6b8
Briana Endanawas <brianamay48@gmail.com>
2021-11-14 00:33:15 -0500

    testing with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,19 @@
-const assertEqual = require('../assertEqual'); 
+const assert = require('chai').assert;
 const tail = require('../tail');
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result.length, 2));
-console.log(assertEqual(result[0], "Lighthouse"));
-console.log(assertEqual(result[1], "Labs"));
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-console.log(assertEqual(words.length, 3)); 
+describe("#head", () => {
+  it("returns 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(result.length, 2);
+  });
+
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(result[0], "Lighthouse");
+  });
+
+  it("returns 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(result[1], "Labs");
+  });
+
+}); 
