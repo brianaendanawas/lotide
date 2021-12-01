b044b8c58760f445de858011284605397b1d1525
Briana Endanawas <brianamay48@gmail.com>
2021-12-01 06:08:25 -0500

    fixing tests

--- without.js
+++ without.js
@@ -1,11 +1,16 @@
-let errorMsg;
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    errorMsg = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-    console.log(errorMsg);
-  } else if (actual === expected) {
-    errorMsg = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-    console.log(errorMsg);
+const eqArrays = function(firstArray, secondArray) {
+  if (JSON.stringify(firstArray) === JSON.stringify(secondArray)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else if (eqArrays(array1, array2) === false) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
@@ -14,17 +19,12 @@ const without = function(source, itemsToRemove) {
   newArray = newArray.filter( function( items ) {
     return !itemsToRemove.includes( items );
   });
-  //console.log(newArray);
   return newArray;
 };
 
-//console.log(without([1, 2, 3], [1]));
-//without([1, 2, 3], [1]);
-//without(["1", "2", "3"], [1, 2, "3"]);
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
 
-//const words = ["hello", "world", "lighthouse"];
-//without(words, ["lighthouse"]); 
-//assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]);
-assertEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
