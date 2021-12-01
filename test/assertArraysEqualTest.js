887953ecbb7b0d326acbc04fd912c4671ba5306a
Briana Endanawas <brianamay48@gmail.com>
2021-12-01 05:24:21 -0500

    fixing tests

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,12 +1,3 @@
-/*
-const assertEqual = require('../assertEqual'); 
-const eqArrays = require('../eqArrays'); 
-
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); */
-
 const assertArraysEqual = require('../assertArraysEqual');
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
